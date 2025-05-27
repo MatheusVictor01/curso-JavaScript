11e574009acccaacfe62d5c1b52fd4913491fa35
Matheus Victor Costa Nunes <matheus.nunes.academy@gmail.com>
2025-05-26 22:34:05 -0400

    material do curso de JavaScritp

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,9 @@
+//Console.log("Trabalhando com variaveis.)
+console.log("trabalhando com variavéis.");
+// JS é case sensitive
+
+const idade = 29;
+console.log(idade);
+
+// não fazer 
+// mes = "Janeiro";
---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,15 @@
+console.log("Trabalhando com Atribuição de variáveis");
+const idade = 29;
+let nome = "Matheus";
+const sobrenome = "Victor";
+
+//console.log(nome + " " + sobrenome);
+console.log(nome, sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+let contador = 0;
+contador = contador +1;
+
+const nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
+nomeCompleto = 2;
