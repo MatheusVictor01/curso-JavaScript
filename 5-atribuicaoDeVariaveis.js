56be27d50d7fc8193cfe13a55489f2c590e6f91b
Matheus Victor Costa Nunes <matheus.nunes@seinf.intra>
2025-05-28 08:49:57 -0400

    Atualizaçoes sobre o curos - listas

--- 5-atribuicaoDeVariaveis.js
+++ 5-atribuicaoDeVariaveis.js
@@ -1,15 +1,19 @@
 console.log("Trabalhando com Atribuição de variáveis");
-const idade = 29;
-let nome = "Matheus";
+
+let primeiroNome = "Matheus";
 const sobrenome = "Victor";
 
 //console.log(nome + " " + sobrenome);
-console.log(nome, sobrenome);
-console.log(`Meu nome é ${nome} ${sobrenome}`);
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
 
 let contador = 0;
 contador = contador +1;
 
-const nomeCompleto = nome + sobrenome;
+const nomeCompleto = primeiroNome + sobrenome;
 console.log(nomeCompleto);
-nomeCompleto = 2;
+
+let idade; //declarando variável
+idade = 26; //atribuindo valor
+idade = idade +1;
+console.log(idade);
